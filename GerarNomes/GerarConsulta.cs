eb88340af7a8d44d68436fc0a341bf1070f9c499
M4rcondess <119372951+M4rcondess@users.noreply.github.com>
2023-05-22 21:32:42 -0300

    Adicionar arquivos de projeto.

---
+++ GerarNomes/GerarConsulta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerarConsulta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Clean up any resources being used
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null ))
+
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private Button button1;
+        private DataGridView dataGridView1;
+       
+
+    }
+}
---
+++ GerarNomes/Pessoas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerarNomes
+{
+    public class Pessoas
+    { 
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+
+        public DateTime datanascimento { get; set; }
+
+        public object email { get; set; }
+
+        public string cpf { get; set; }
+
+        public object fone { get; set; }
+
+        public string sexo { get; set; }
+
+        public string estadocivil { get; set; }
+
+        public object hobbie { get; set; }
+
+        public int idade { get; set; }
+
+
+
+
+    }
+}
